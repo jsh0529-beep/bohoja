d6b0973348e925786c5835c639a448bd2ac91550
jsh0529-beep <jsh0529@gmail.com>
2026-08-08 22:07:50 +0900

    검수 임시 파일 제외 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,4 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
-export default defineConfig([...nextVitals, ...nextTs, { rules: { "@next/next/no-html-link-for-pages": "off" } }, globalIgnores([".next/**", "coverage/**", "public/tesseract/**"])]);
+export default defineConfig([...nextVitals, ...nextTs, { rules: { "@next/next/no-html-link-for-pages": "off" } }, globalIgnores([".next/**", "coverage/**", "work/**", "public/tesseract/**"])]);
